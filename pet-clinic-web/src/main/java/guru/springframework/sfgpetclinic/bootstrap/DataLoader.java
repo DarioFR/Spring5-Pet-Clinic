7f4ec25f1f14c7208a55e57f513e6714b1567526
dario <me@example.com>
2018-12-13 23:07:34 +0100

    added comments on DataLoader, added Owners list and updated controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,22 +4,29 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 //implementando CommandLineRunner e marcandolo come component questa classe verrà runnata da Spring automaticamente
-// quando il contesto Spring si sarà caricato.
+// quando il contesto Spring si sarà caricato (viene difatti chiamato il metodo run).
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    //i due service vengono iniettati mediante constructor injection, ma è necessario
+    // annotare i singoli service con @Service affinchè la component Scan li individui come beans
+    //da inserire nel lifecycle di Spring. La component scan è definita implicitamente a partire dal package
+    //guru.springframework.sfgpetclinic della pet-clinic-web e si estende ai sotto package.
+    //La component scan funziona però anche sul progetto pet-clinic-data a patto che venga mantenuto
+    //esattamente lo stesso nome package  di prima, dato che nel packaging finale della app
+    //il progetto pet-clinic-data verrà tirato dentro come dipendenza di pet-clinic-web
+    // portando dentro quindi anche le classi compilate relative a pet-clinic-data, mantenendo come
+    //package guru.springframework.sfgpetclinic.
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
 
